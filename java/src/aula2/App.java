98612ad69479d47a55a1712fe001c943023c733b
thaysferica <thaynara.couto.pereira@escola.pr.gov.br>
2025-06-30 12:27:37 -0300

    kk

---
+++ java/src/aula2/App.java
@@ -0,0 +1,13 @@
+package aula2;
+
+public class App {
+    public static void main(String[] args) {
+        int b;
+        String a;
+        
+        a = (b == 5)? "b é igual a 5": "b é diferente de 5";
+
+        System.out.println("resultado" + a);
+
+    }
+}
