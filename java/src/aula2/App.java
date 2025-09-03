d4a121c0fe23d9eccceccfdae483a7f085da3940
thaysferica <thaynara.couto.pereira@escola.pr.gov.br>
2025-09-03 11:50:19 -0300

    tht
    gy

--- java/src/aula2/App.java
+++ java/src/aula2/App.java
@@ -1,12 +1,1 @@
-package aula2;
 
-public class App {
-    public static void main(String[] args) {
-        int b = 0;  // Inicializando b com um valor
-        String a;
-        
-        a = (b == 5) ? "b é igual a 5" : "b é diferente de 5";
-
-        System.out.println("resultado: " + a);
-    }
-}
