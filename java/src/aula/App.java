fb8a6c40ad599d1c75933ee5992d59347537dc8c
thaysferica <thaynara.couto.pereira@escola.pr.gov.br>
2025-09-10 11:53:19 -0300

    yf

---
+++ java/src/aula/App.java
@@ -0,0 +1,14 @@
+package aula;
+
+public class App {
+   public static void main(String[] args) {
+String nome;
+String descricao;
+String autor;
+String isbn;
+double preco;
+String datapub;
+
+   }
+}
+
